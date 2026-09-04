a8506dba1b82ca5546e6b49d42c6a0b4d0a86c48
Tomaž Jurše <jursetomaz@gmail.com>
2026-09-04 20:15:57 +0200

    Add /cene endpoint with JOIN across table

--- backend/main.py
+++ backend/main.py
@@ -13,3 +13,14 @@ def get_trgovine(db: Session = Depends(get_db)):
 @app.get("/izdelki", response_model=list[schemas.IzdelekOut])
 def get_izdelki(db: Session = Depends(get_db)):
     return db.query(models.Izdelek).all()
+
+@app.get("/cene", response_model=list[schemas.CenaDetajlOut])
+def get_cene(db: Session = Depends(get_db)):
+    return db.query(
+        models.Izdelek.ime.label("ime_izdelek"),
+        models.Trgovina.ime.label("ime_trgovina"),
+        models.Cena.cena,
+        models.Cena.datum_zajema
+    ).join(models.Izdelek, models.Cena.izdelek_id == models.Izdelek.id
+    ).join(models.Trgovina, models.Cena.trgovina_id == models.Trgovina.id
+    ).all()
